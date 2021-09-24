57a298f7638dd268230277f29e50d7a0179cc0d0
blackboardd <86866786+blackboardd@users.noreply.github.com>
2021-09-24 16:37:45 -0700

    fix: disabled some css attributes globally
    They were making it harder to style components and because I intend
    to make a library of the components I figure I will use, it makes
    sense to clear attributes I commonly modify.

---
+++ src/globalStyle.ts
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    border-radius: 0;
+    border-color: 0;
+    border-style: 0;
+    border-width: 0;
+    padding: 0;
+    margin: 0;
+  }
+`;
+
+export default GlobalStyle;
