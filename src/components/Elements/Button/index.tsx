2fae5949cbf2a34d456121a5c758b34db9da44da
blackboardd <86866786+blackboardd@users.noreply.github.com>
2021-09-24 19:58:13 -0700

    refactor: Button modified for common interactive
    See src/components/Common/Interactive.

--- src/components/Elements/Button/index.tsx
+++ src/components/Elements/Button/index.tsx
@@ -1,72 +1,35 @@
-import React, { HTMLAttributes, useEffect, useState } from 'react';
-import { theme } from '../../../theme';
+import React, { HTMLAttributes, useState } from 'react';
+import { speed } from '../../../theme';
+import Wipe from '../../Common/Interactive/Wipe';
 import { ButtonProps } from './interface';
-import { Line, StyledButton, StyledClickOverlay, SvgOverlay } from './styles';
+import { StyledButton } from './styles';
 
 // TODO: Animate button with React-spring
 export default (props: HTMLAttributes<HTMLButtonElement> & ButtonProps) => {
   const [border, setBorder] = useState(false);
   const [wipe, setWipe] = useState(false);
-  useEffect(() => {
-    return () => {
-      const int = setInterval(() => {
-        setWipe(true);
-        clearInterval(int);
-      }, 3);
-    };
-  }, [wipe]);
+  const handleClick = () => {
+    setBorder(false);
+    setWipe(false);
+    setBorder(true);
+    setWipe(true);
+    const int = setInterval(() => {
+      setBorder(false);
+      setWipe(false);
+      clearInterval(int);
+    }, speed.interactive.hyper * 1000);
+  };
   const { children, primary } = props;
+
   return (
-    <div>
-      <StyledButton
-        onClick={() => {
-          setBorder(false);
-          setWipe(false);
-          setBorder(!border);
-          setWipe(!wipe);
-          const int = setInterval(() => {
-            setBorder(false);
-            clearInterval(int);
-          }, 200);
-        }}
-        border={border}
-        wipe={wipe}
-        primary={primary}
-      >
-        <div>{children}</div>
-        <StyledClickOverlay>
-          <SvgOverlay
-            border={border}
-            version='1.1'
-            xmlns='http://www.w3.org/2000/svg'
-            xmlnsXlink='http://www.w3.org/1999/xlink'
-            x='0px'
-            y='0px'
-            width='100px'
-            height='100px'
-            viewBox='0 0 100 100'
-            preserveAspectRatio='none'
-            enable-background='new 0 0 100 100'
-            xmlSpace='preserve'
-          >
-            <defs>
-              <filter id='filter'>
-                <feGaussianBlur stdDeviation='9' />
-              </filter>
-            </defs>
-            <Line
-              d='m88.31776,92.61682l-83.08411,-82.8972c0.09346,0.09346 0.28037,-6.26168 6.26168,-3.45795c5.98131,2.80374 81.86916,81.86916 81.7757,81.7757c0.09346,0.09346 -0.09346,4.39252 -4.95327,4.57944z'
-              border={border}
-              wipe={wipe}
-              x='95%'
-              width='5%'
-              height='100%'
-              filter='url(#filter)'
-              fill={theme.palette.focus.highlight}
-            />
-          </SvgOverlay>
-        </StyledClickOverlay>
-      </StyledButton>
-    </div>
+    <StyledButton
+      onClick={handleClick}
+      border={border}
+      wipe={wipe}
+      primary={primary}
+    >
+      {children}
+      <Wipe border={border} wipe={wipe} />
+    </StyledButton>
   );
 };
--- src/components/Elements/Button/styles.ts
+++ src/components/Elements/Button/styles.ts
@@ -1,28 +1,22 @@
-import { darken } from 'polished';
 import styled from 'styled-components';
 import {
+  border,
   borderRadius,
   borderThickness,
-  font,
-  gray,
-  highlight,
   primary,
   secondary,
-  shadows,
   spacing,
-  speed,
   typography,
   weight,
 } from './../../../theme';
+import { Interactive } from './../../Common/Interactive/styles';
 import { ButtonProps } from './interface';
 
 export const StyledButton = styled('button')<ButtonProps>`
+  ${Interactive}
   z-index: 0;
   position: relative;
   border-radius: ${(p) => (p.border ? borderRadius.small : borderRadius.base)};
-  transition: border-radius ${speed.interactive.hyper} ease-in-out,
-    background-color ${speed.interactive.hyper}s ease-in-out,
-    box-shadow ${speed.interactive.hyper} ease-in-out;
   letter-spacing: 0.5px;
   text-align: center;
   padding-top: auto;
@@ -35,46 +29,10 @@ export const StyledButton = styled('button')<ButtonProps>`
   margin-right: ${spacing.medium};
   height: ${spacing.large};
   width: auto;
-  font-family: ${font.base};
   font-weight: ${weight.regular};
   border-style: solid;
   border-width: ${borderThickness.base};
-  border-color: ${gray[5]};
-  &:focus {
-    border-color: unset;
-    outline: ${borderThickness.medium} solid ${highlight};
-  }
-  &:hover {
-    background-color: ${(p) =>
-      p.primary ? darken(0.01, primary) : darken(0.01, secondary)};
-    box-shadow: ${shadows.box.soft.small};
-  }
+  border-color: ${border.color};
   color: ${(p) => (p.primary ? typography.primary : typography.secondary)};
   background-color: ${(p) => (p.primary ? primary : secondary)};
-`;
-
-export const SvgOverlay = styled('svg')<ButtonProps>`
-  border-radius: ${(p) => (p.border ? borderRadius.small : borderRadius.base)};
-  position: absolute;
-  height: 140%;
-  width: 110%;
-  left: -5px;
-  top: -5px;
-`;
-
-export const StyledClickOverlay = styled('div')<ButtonProps>`
-  position: absolute;
-  height: 100%;
-  width: 100%;
-  z-index: -1;
-  left: 0;
-  top: 0;
-`;
-
-export const Line = styled('path')<ButtonProps>`
-  transition: ease-in-out ${(p) => (p.wipe ? speed.interactive.mild : '0s')} transform;
-  transform: translate(
-    ${(p) => (p.wipe ? '100%' : '-100%')},
-    ${(p) => (p.wipe ? '-100%' : '100%')}
-  );
 `;
