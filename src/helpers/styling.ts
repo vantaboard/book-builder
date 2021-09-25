854027b7d6bcacf2fbe894817f405ea3aa0b3db9
blackboardd <86866786+blackboardd@users.noreply.github.com>
2021-09-24 19:52:06 -0700

    refactor: moved ignoreClicks helper into module

---
+++ src/helpers/styling.ts
@@ -0,0 +1,8 @@
+export const ignoreClicks = () => `
+  -webkit-touch-callout: none;
+  -webkit-user-select: none;
+  -khtml-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+`;
